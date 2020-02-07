a87df0a1f853f4e3cf8a671339a264619febf215
Jim Chen <cqz@cs.washington.edu>
2020-02-06 16:55:23 -0800

    Add biblio script

---
+++ js/biblio.js
@@ -0,0 +1,48 @@
+/**
+ * Old fashioned biblio loader
+ * @author Jim Chen
+ */
+
+var BiblioLoader = (function (tags) {
+  function BiblioLoader(tagset) {
+    this.tagset = (typeof tagset === 'undefined') ? tags : tagset;
+  }
+
+  BiblioLoader.prototype.load = function () {
+    return fetch('https://www.cs.washington.edu/_webservices/publications/json.php', {'mode': 'cors'})
+      .then(function(response) {
+        return response.json().map(function (paper) {
+          return {
+            'title': paper['title'],
+            'sort_title': paper[''],
+            'venue': paper['biblio_secondary_title'],
+            'time': {
+              'year': paper['biblio_year'],
+              'month': paper['biblio_date']
+            },
+            'url': paper['biblio_url'],
+            'type': paper['biblio_type_name'],
+            'authors': paper['biblio_contributors'].map(function (author) {
+              return {
+                'name': author['name'],
+                'affiliation': author['affiliation'],
+                'rank': parseInt(author['rank'], 10)
+              };
+            })
+          };
+        });
+      })
+      .catch(function(error) {
+        console.error('Request failed', error);
+        throw error;
+      });
+  };
+
+  return BiblioLoader;
+})(["crowdlab",
+    "ai-crowd-train-test",
+    "crowd-argumentation",
+    "ai-bootstrapping-communities",
+    "ai-crowd-task-routing",
+    "ai-crowd-multi-label",
+    "crowd-task-design"]);
