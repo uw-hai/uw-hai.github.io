2955ef188ed821df385493b9a77aa47a96473832
Jim Chen <cqz@cs.washington.edu>
2020-02-06 17:32:38 -0800

    Add render support

--- js/biblio.js
+++ js/biblio.js
@@ -3,9 +3,59 @@
  * @author Jim Chen
  */
 
-var BiblioLoader = (function (tags) {
+var BiblioLoader = (function (defaultTags, defaultGroupOrder) {
+  var MONTHS = {
+    'jan': 0.00,
+    'feb': 0.01,
+    'mar': 0.02,
+    'apr': 0.03,
+    'may': 0.04,
+    'jun': 0.05,
+    'jul': 0.06,
+    'aug': 0.07,
+    'sep': 0.08,
+    'oct': 0.09,
+    'nov': 0.10,
+    'dec': 0.11,
+    '': 0.12
+  };
+  
+  function getSimpleDate(year, month) {
+    return parseInt(year, 10) +
+      MONTHS[month.trim().substring(0, 3).toLowerCase()];
+  }
+  
+  function _paperComparator(a, b) {
+    var time_a = getSimpleDate(a['time']['year'], a['time']['date']),
+        time_b = getSimpleDate(b['time']['year'], b['time']['date']);
+    if (time_a < time_b) {
+      return -1
+    } else if (time_a > time_b) {
+      return 1;
+    } else {
+      if (a['sort_title'] > b['sort_title']) {
+        return 1;
+      } else if (a['sort_title'] < b['sort_title']) {
+        return -1;
+      }
+      return 0;
+    }
+  }
+
+  function _paperRenderer(_, paper) {
+    return _('tr', {}, [
+      _('td', {}, [
+        _('span', { 'className': 'date' }, [
+          _('big', {}, [_('strong', {}, [_('', paper['time']['year'])])]),
+          _('', paper['time']['date'])
+        ])
+      ]),
+      _('td', { 'className': 'publication' }, []),
+    ]);
+  }
+
   function BiblioLoader(tagset) {
-    this.tagset = (typeof tagset === 'undefined') ? tags : tagset;
+    this.tagset = (typeof tagset === 'undefined') ? defaultTags : tagset;
   }
 
   BiblioLoader.prototype.load = function () {
@@ -20,7 +70,7 @@ var BiblioLoader = (function (tags) {
         return papers.map(function (paper) {
           return {
             'title': paper['title'],
-            'sort_title': paper[''],
+            'sort_title': paper['biblio_sort_title'],
             'venue': paper['biblio_secondary_title'],
             'time': {
               'year': paper['biblio_year'],
@@ -44,6 +94,38 @@ var BiblioLoader = (function (tags) {
       });
   };
 
+  BiblioLoader.prototype.loadGrouped = function () {
+    return this.load().then(function (papers) {
+      var groups = {};
+      papers.forEach(function (paper) {
+        if (!(paper['biblio_type_name'] in groups)) {
+          groups[paper['biblio_type_name']] = [];
+        }
+        groups[paper['biblio_type_name']].push(paper);
+      });
+      // Sort the groups by publication time then title
+      for (var group in groups) {
+        groups[group].sort(_paperComparator);
+      }
+      return groups;
+    });
+  };
+
+  BiblioLoader.prototype.createBiblioDOM = function (parent, _, groupOrder) {
+    if (typeof groupOrder === 'undefined' || groupOrder === null) {
+      return this.load.then(function (papers) {
+        papers.sort(_paperComparator);
+        return _('table', {}, papers.map(function (paper) {
+          return _paperRenderer(_, paper);
+        }));
+      });
+    } else {
+      return this.loadGrouped.then(function (grouped) {
+        console.log('not implemented');
+      });
+    }
+  };
+
   return BiblioLoader;
 })(["crowdlab",
     "ai-crowd-train-test",
@@ -51,4 +133,5 @@ var BiblioLoader = (function (tags) {
     "ai-bootstrapping-communities",
     "ai-crowd-task-routing",
     "ai-crowd-multi-label",
-    "crowd-task-design"]);
+    "crowd-task-design"], 
+   null);
---
+++ js/dfc.js
@@ -0,0 +1,60 @@
+/* DFC - Minimal JS DOM Manipulator */
+var DFC = (function () {
+  'use strict';
+  const SVG_TYPES = [
+    'svg', 'rect', 'circle', 'ellipse', 'path', 'g'
+  ];
+  var dfc = function (type, props, children, callback) {
+    // Initialize the element 
+    var elem = null;
+    if (type instanceof Element || type instanceof HTMLDocument) {
+      elem = type;
+    } else if (typeof type === 'string') {
+      type = type.toLowerCase().trim();
+      if (type === '') {
+        return document.createTextNode(props);
+      } else if (SVG_TYPES.indexOf(type) >= 0) {
+        elem = document.createElementNS("http://www.w3.org/2000/svg", type);
+      } else {
+        elem = document.createElement(type);
+      }
+    } else {
+      throw new Error('First argument must be a string or Element.');
+    }
+
+    if (typeof props !== 'object' || props == null) {
+      props = {};
+    }
+
+    // Set the properties for the element
+    for (var propName in props) {
+      if (propName === 'style' ){
+        for (var styleName in props[propName]) {
+          elem.style[styleName] = props[propName][styleName];
+        }
+      } else if (propName === 'className') {
+        elem.className = props[propName];
+      } else {
+        elem.setAttribute(propName, props[propName]);
+      }
+    }
+    
+    if (Array.isArray(children)) {
+      // We have child elements to add
+      for (var i = 0; i < children.length; i++) {
+        if (children[i] !== null) {
+          elem.appendChild(children[i]);
+        }
+      }
+    }
+
+    if (typeof callback === 'function') {
+      // Do post-processing on the element
+      callback(elem);
+    }
+
+    return elem;
+  };
+
+  return dfc;
+})();
